90834708d8e9ce49c4a943c85ba56951bfa2e560
Umar Solanki <uysolanki@gmail.com>
2024-11-12 19:53:12 +0530

    CRD methods

--- src/main/java/com/excelr/ProjectCustomerManagementSystem/controller/CustomerController.java
+++ src/main/java/com/excelr/ProjectCustomerManagementSystem/controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,63 +35,35 @@ public class CustomerController {
 		return "Welcome to Chennai super Kings";
 	}
 	
-	@RequestMapping("/getCustomer")
-	public Customer getCustomer()
+	@RequestMapping("/getCustomer/{custId}")
+	public Customer getCustomer(@PathVariable("custId") int cno)
 	{
-		Customer customer=new Customer();
-		customer.setCno(1);
-		customer.setEmail("alice@gmail.com");
-		customer.setFirstname("Alice");
-		customer.setGender("female");
-		customer.setLastname("Fernandes");
-		customer.setMobileno("9890123123");
-		return customer;
+		return customerService.getCustomer(cno);
 	}
 	
 	@RequestMapping("/getAllCustomers")
 	public List<Customer> getAllCustomers()
 	{
-		Customer customer1=new Customer();
-		customer1.setCno(1);
-		customer1.setEmail("virat@gmail.com");
-		customer1.setFirstname("Virat");
-		customer1.setGender("male");
-		customer1.setLastname("Kohli");
-		customer1.setMobileno("9890123333");
-		
-		Customer customer2=new Customer();
-		customer2.setCno(2);
-		customer2.setEmail("alice@gmail.com");
-		customer2.setFirstname("Alice");
-		customer2.setGender("female");
-		customer2.setLastname("Fernandes");
-		customer2.setMobileno("9890123123");
-		
-		Customer customer3=new Customer();
-		customer3.setCno(3);
-		customer3.setEmail("rohit@gmail.com");
-		customer3.setFirstname("Rohit");
-		customer3.setGender("male");
-		customer3.setLastname("Sharma");
-		customer3.setMobileno("9890128888");
-		
-		List<Customer> customers=new ArrayList();
-		customers.add(customer1);
-		customers.add(customer2);
-		customers.add(customer3);
-		return customers;
+			return customerService.getAllCustomers();
 	}
 	
 	@RequestMapping("/saveCustomer")
 	public String saveCustomer()
 	{
 		Customer customer=new Customer();
-		customer.setEmail("ben@gmail.com");
-		customer.setFirstname("Ben");
-		customer.setGender("male");
-		customer.setLastname("Smith");
-		customer.setMobileno("8888123123");
+		customer.setEmail("alice@gmail.com");
+		customer.setFirstname("Alice");
+		customer.setGender("female");
+		customer.setLastname("Fernandes");
+		customer.setMobileno("9890123123");
 		customerService.saveCustomer(customer);
 		return "Customer Record Added";
+	}
+	
+	@RequestMapping("/deleteCustomer/{custId}")
+	public String deleteCustomer(@PathVariable("custId") int cno)
+	{
+		customerService.deleteCustomer(cno);
+		return "Customer Deleted";
 	}
 }
--- src/main/java/com/excelr/ProjectCustomerManagementSystem/service/CustomerService.java
+++ src/main/java/com/excelr/ProjectCustomerManagementSystem/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.excelr.ProjectCustomerManagementSystem.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,16 @@ public class CustomerService {
 	CustomerRepository customerRepository;
 	public void saveCustomer(Customer customer) {
 		customerRepository.save(customer);
+	}
+	public List<Customer> getAllCustomers() {
+		return customerRepository.findAll();
+	}
+	public void deleteCustomer(int cno) {
+		customerRepository.deleteById(cno);	
+	}
+	public Customer getCustomer(int cno) {
+		return customerRepository.findById(cno).get();
+		
 	}
 
 }
